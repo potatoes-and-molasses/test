e096b738edc7c581401ac320302be73e49643fea
YoavPinhas <78155124+YoavPinhas@users.noreply.github.com>
2021-01-29 00:28:22 +0200

    Dogs Logic improvement

--- doge savior tycoon/Assets/Scripts/DogMovement.cs
+++ doge savior tycoon/Assets/Scripts/DogMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 //[RequireComponent(typeof(LineRenderer))]
 public class DogMovement : MonoBehaviour
 {
+    enum State {WalksToOwner, WalksToRandomPoint, RichedRandomPoint , RichedOwner };
     public Transform owner;
     public float lishLength = 5;
     public float minDistanceFromOwner = 1;
@@ -17,8 +18,11 @@ public class DogMovement : MonoBehaviour
 
     private Vector2 velocity;
     private float currentSpeed;
-    private bool isMoving;
+    private bool goToOwner;
     private Rigidbody2D rb;
+    private Vector3 targetPosition;
+    private State state = State.RichedRandomPoint;
+    private float timeNearTarget = 0;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -28,59 +32,120 @@ public class DogMovement : MonoBehaviour
         lish.startColor = lishColor;
         lish.endColor = lishColor;
         lish.sortingOrder = -1;
+        GenerateNewTarget();
+        speed = Random.Range(4, 10);
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        var dist = Vector3.Distance(transform.position, owner.position);
-
-        if(dist <= lishLength)
-            isMoving = false;
-        else if(!isMoving && dist >= lishLength)
-            isMoving = true;
-        UpdateSpeed(dist);
-        LookAtOwner();
-        CalculateVelocity();
-        Move(dist);
+        var distFromOwner = Vector3.Distance(transform.position, owner.position);
+        var distFromTarget = Vector3.Distance(transform.position, targetPosition);
+        DogsLogic(distFromOwner, distFromTarget);
         lish.SetPositions(new Vector3[] { transform.position, owner.position });
     }
 
+
+    void DogsLogic(float distanceToOwner, float distanceToTargrt)
+    {
+        switch (state)
+        {
+            case State.WalksToOwner:
+                if(distanceToOwner <= minDistanceFromOwner)
+                {
+                    state = State.RichedOwner;
+                    return;
+                }
+                UpdateMovement(distanceToTargrt);
+                return;
+            case State.WalksToRandomPoint:
+                if (distanceToOwner > lishLength)
+                {
+                    state = State.WalksToOwner;
+                    return;
+                }
+                if(distanceToTargrt < 0.3f)
+                {
+                    state = State.RichedRandomPoint;
+                    timeNearTarget = 0;
+                    return;
+                }
+                UpdateMovement(distanceToTargrt);
+                return;
+            case State.RichedRandomPoint:
+                if(timeNearTarget >= 1)
+                {
+                    state = State.WalksToRandomPoint;
+                    GenerateNewTarget();
+                    return;
+                }
+                timeNearTarget += Time.fixedDeltaTime;
+                return;
+            case State.RichedOwner:
+                GenerateNewTarget();
+                state = State.WalksToRandomPoint;
+                return;
+        }
+    }
+
+    void UpdateMovement(float distFromTarget)
+    {
+        UpdateSpeed(distFromTarget);
+        CalculateVelocity();
+        LookAtTarget();
+        Move(distFromTarget);
+        
+    }
     void UpdateSpeed(float dist)
     {
-        if (isMoving)
+        if (dist < 0.5f)
             currentSpeed = Mathf.Clamp(currentSpeed + acceleration * Time.fixedDeltaTime, 0.3f, speed);
         else
+            currentSpeed = Mathf.Clamp(currentSpeed - deceleration * Time.fixedDeltaTime, 0, currentSpeed);   
+    }
+
+    void GenerateNewTarget()
+    {        
+        var length = Random.Range(minDistanceFromOwner, lishLength);
+        var angle = Random.Range(0, 2 * Mathf.PI);
+        Vector3 newTarget = owner.position + new Vector3(Mathf.Sin(angle) * length, Mathf.Cos(angle) * length, 0);
+        while(Vector3.Distance(newTarget, targetPosition) < minDistanceFromOwner/2)
         {
-            if(dist < minDistanceFromOwner)
-                currentSpeed = Mathf.Clamp(currentSpeed - deceleration * Time.fixedDeltaTime, 0, currentSpeed);
+            length = Random.Range(minDistanceFromOwner, lishLength);
+            angle = Random.Range(0, 2 * Mathf.PI);
+            newTarget = owner.position + new Vector3(Mathf.Sin(angle) * length, Mathf.Cos(angle) * length, 0);
         }
-        
+        targetPosition = newTarget;
     }
 
     void CalculateVelocity()
     {
-        velocity = (owner.position - transform.position).normalized;
+        if(state == State.WalksToOwner)
+            velocity = (owner.position - transform.position).normalized;
+        else
+            velocity = (targetPosition - transform.position).normalized;
     }
-    void LookAtOwner()
+    void LookAtTarget()
     {
-
-        var lookVector = (owner.position - transform.position).normalized;
-        
-        var angle = (lookVector.y >= 0) ? Mathf.Acos(lookVector.x) * Mathf.Rad2Deg : -Mathf.Acos(lookVector.x) * Mathf.Rad2Deg;
+        var angle = (velocity.y >= 0) ? Mathf.Acos(velocity.x) * Mathf.Rad2Deg : -Mathf.Acos(velocity.x) * Mathf.Rad2Deg;
 
         transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), turnSpeed * Time.fixedDeltaTime);
     }
     void Move(float dist)
     {
         var newPos = new Vector3(velocity.x, velocity.y, 0);
-        if (isMoving)
-          rb.MovePosition(transform.position + newPos * currentSpeed * Time.fixedDeltaTime);
-        else
-        {
-            var distDiff = Mathf.Clamp(dist - minDistanceFromOwner, 0.1f, 1);
-            rb.MovePosition(transform.position + newPos * distDiff * currentSpeed * Time.fixedDeltaTime);
-        }
+        if(state == State.WalksToOwner)
+            rb.MovePosition(transform.position + newPos * speed * Time.fixedDeltaTime);
+        else if(dist > 0.3)
+            rb.MovePosition(transform.position + newPos * (speed/2) * Time.fixedDeltaTime);
     }
 
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawSphere(targetPosition, 1);
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(owner.position, minDistanceFromOwner);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(owner.position, lishLength);
+    }
 }
