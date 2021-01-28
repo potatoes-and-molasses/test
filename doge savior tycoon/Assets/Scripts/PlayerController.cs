7459f27fa784a52eae28891020a41815639755c7
YoavPinhas <78155124+YoavPinhas@users.noreply.github.com>
2021-01-28 19:03:42 +0200

    More work on the player
    Change the player controll code
    delete unnecessary scripts

--- doge savior tycoon/Assets/Scripts/PlayerController.cs
+++ doge savior tycoon/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10;
+    public float acceleration = 5;
+    public float deceleration = 8;
+    public float speed = 6;
+    public float turnSpeed = 10;
     
-    Inputs inputs;
-    Vector2 velocity;
+    private Inputs inputs;
+    private Vector2 velocity;
+    private float currentSpeed;
+    private bool isMoving;
+    private Rigidbody2D rb;
 
     private void Awake()
     {
@@ -26,12 +32,40 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
-        inputs.KeysandMouse.Movement.performed += ctx => velocity = ctx.ReadValue<Vector2>();
+        rb = GetComponent<Rigidbody2D>();
+        inputs.KeysandMouse.Movement.performed += ctx => 
+        { 
+            velocity = ctx.ReadValue<Vector2>();
+            isMoving = true;
+        };
+        inputs.KeysandMouse.Movement.canceled += ctx =>
+        {
+            //velocity = Vector2.zero;
+            isMoving = false;
+        };
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        transform.position += new Vector3(velocity.x, velocity.x,0) * speed * Time.deltaTime;
+        UpdateSpeed();
+        Move();
     }
+
+    void UpdateSpeed()
+    {
+        if(isMoving)
+            currentSpeed = Mathf.Clamp(currentSpeed + acceleration * Time.fixedDeltaTime, 0, speed);
+        else
+            currentSpeed = Mathf.Clamp(currentSpeed - deceleration * Time.fixedDeltaTime, 0, speed);
+    }
+
+    void Move()
+    {
+        var newPos = transform.position + new Vector3(velocity.x, velocity.y, 0) * currentSpeed * Time.fixedDeltaTime;
+        var angle = (velocity.y >=0)? Mathf.Acos(velocity.x) * Mathf.Rad2Deg : -Mathf.Acos(velocity.x) * Mathf.Rad2Deg;
+        rb.MoveRotation(Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, angle), turnSpeed * Time.fixedDeltaTime));
+        rb.velocity = velocity * currentSpeed;
+    }
+
 }
