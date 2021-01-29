4213e9069bef0caadfe22d42189a61e5692275bb
YoavPinhas <78155124+YoavPinhas@users.noreply.github.com>
2021-01-29 18:32:19 +0200

    Changes
    Add victims,
    Improve doges,
    add Game manager

--- doge savior tycoon/Assets/Scripts/CameraController.cs
+++ doge savior tycoon/Assets/Scripts/CameraController.cs
@@ -6,16 +6,20 @@ public class CameraController : MonoBehaviour
 {
 
     public float followSpeed = 10;
+    Vector3 velocity = Vector3.zero;
+    Vector3 lastPos;
+    Camera cam;
     void Start()
     {
-        
+        cam = GetComponent<Camera>();
     }
 
-    private void LateUpdate()
+    private void Update()
     {
-        Vector3 newPos = GameManager.Player.transform.position;
-        newPos.z = transform.position.z;
-        transform.position = Vector3.Slerp(transform.position, newPos, followSpeed * Time.deltaTime);
-        
+         velocity = transform.position - lastPos;
+         lastPos = transform.position;
+         Vector3 newPos = GameManager.Player.transform.position;
+         newPos.z = transform.position.z;
+         transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, 10 * Time.smoothDeltaTime);
     }
 }
